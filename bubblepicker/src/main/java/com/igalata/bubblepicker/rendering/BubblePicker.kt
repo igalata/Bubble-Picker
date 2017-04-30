83183c82c18c6d640ed65818ec05e8cfda1e0e56
ckcks12 <wyoducc@gmail.com>
2017-04-30 13:32:41 +0900

    background transparent support

--- bubblepicker/src/main/java/com/igalata/bubblepicker/rendering/BubblePicker.kt
+++ bubblepicker/src/main/java/com/igalata/bubblepicker/rendering/BubblePicker.kt
@@ -1,6 +1,7 @@
 package com.igalata.bubblepicker.rendering
 
 import android.content.Context
+import android.graphics.PixelFormat
 import android.opengl.GLSurfaceView
 import android.support.annotation.ColorInt
 import android.util.AttributeSet
@@ -58,8 +59,12 @@ class BubblePicker : GLSurfaceView {
 
     constructor(context: Context?) : this(context, null)
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
+        setZOrderOnTop(true)
         setEGLContextClientVersion(2)
+        setEGLConfigChooser(8, 8, 8, 8, 16, 0)
+        holder.setFormat(PixelFormat.RGBA_8888)
         setRenderer(renderer)
+        setRenderMode(RENDERMODE_WHEN_DIRTY)
         renderMode = RENDERMODE_CONTINUOUSLY
         attrs?.let { retrieveAttrubutes(attrs) }
     }
